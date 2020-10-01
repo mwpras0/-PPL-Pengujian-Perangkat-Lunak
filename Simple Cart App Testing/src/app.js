b0cfd15236a3f09937585789b48b68b129b27bb9
mwpras0 <mwpras0@gmail.com>
2020-10-01 22:23:48 +0700

    Simple Card App Testing

---
+++ Simple Cart App Testing/src/app.js
@@ -0,0 +1,37 @@
+import {
+    incrementQty,
+    decrementQty,
+    subTotal
+} from './helpers.js';
+
+const incrButton = document.querySelector('#incr');
+const decrButton = document.querySelector('#decr');
+const qtyInput = document.querySelector('#qty');
+const textSubTotal = document.querySelector('#subtotal');
+const priceInput = document.querySelector("#price");
+const discInput = document.querySelector('#code');
+const discButton = document.querySelector('#confirmButton');
+
+incrButton.addEventListener('click', () => {
+    qtyInput.value = incrementQty(qtyInput.value);
+    textSubTotal.textContent = `Rp. ${subTotal(priceInput.value, qtyInput.value, discInput.value)}`;
+});
+
+decrButton.addEventListener('click', () => {
+    if (parseInt(qtyInput.value) > 1) {
+        qtyInput.value = decrementQty(qtyInput.value);
+        textSubTotal.textContent = `Rp. ${subTotal(priceInput.value, qtyInput.value, discInput.value)}`;
+    }
+});
+
+discButton.addEventListener('click', () => {
+    textSubTotal.textContent = `Rp. ${subTotal(priceInput.value, qtyInput.value, discInput.value)}`;
+})
+
+priceInput.addEventListener('keyup', () => {
+    textSubTotal.textContent = `Rp. ${subTotal(priceInput.value, qtyInput.value, discInput.value)}`;
+});
+
+qtyInput.addEventListener('keyup', () => {
+    textSubTotal.textContent = `Rp. ${subTotal(priceInput.value, qtyInput.value, discInput.value)}`;
+});
---
+++ Simple Cart App Testing/src/helpers.js
@@ -0,0 +1,9 @@
+const incrementQty = (qty) => parseInt(qty) + 1;
+const decrementQty = (qty) => parseInt(qty) - 1;
+const subTotal = (price, qty, discount) => ((!isNaN(parseInt(price)) && !isNaN(parseInt(qty))) ? parseInt(price) * parseInt(qty) : 0) - ((discount) ? ((!isNaN(parseInt(price)) && !isNaN(parseInt(qty))) ? parseInt(price) * parseInt(qty) : 0) * 0.20 : 0);
+
+module.exports = {
+    incrementQty,
+    decrementQty,
+    subTotal
+};
